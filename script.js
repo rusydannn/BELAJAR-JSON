43f12dce90624ab974b04826f97c399a898234a0
unknown <rusydanzainal25@gmail.com>
2024-12-06 10:02:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+{
+    "mahasiswa": [{
+        "id":1,
+        "name": "Muhammad Rusydan",
+        "email": "rusydanzainal25@gmail.com",
+        "kota": "Depok"
+        "matkul": ["Fisika", "Pemrograman Web", "Algoritma"]
+    },
+    {
+        "id": 2,
+        "name": "Nadya Elrycca",
+        "email": "nadincantik@gmail.com",
+        "kota": "Bandung"
+    }
+    {
+        "id": 3,
+        "name": "Asep rt",
+        "email": "Ganjary@gmail.com",
+        "kota": "Cirebon"
+    }
+    {
+        "id": 4,
+        "name": "Es teh",
+        "email": "Gus@gmail.com",
+        "kota": "Jogja"
+    }
+    {
+        "id": 5,
+        "name": "Bobby",
+        "email": "kertanegara@gmail.com",
+        "kota": "Jakarta"
+    }
+    ]
+}
